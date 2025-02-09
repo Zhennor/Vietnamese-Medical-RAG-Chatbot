44ba80809d73ec02590086c2a1390b3e26f3e735
Tung Le Thanh <139512238+Zhennor@users.noreply.github.com>
2025-02-09 12:11:34 +0700

    update score rerank

--- app/rerank/cohere_rerank.py
+++ app/rerank/cohere_rerank.py
@@ -78,7 +78,7 @@ class Cohere:
 
             combined_scores = 0.8 * scores_cosine + 0.2 * scores_tfidf
 
-            if not rerank_links and all(score < 0.3 for score in combined_scores):
+            if not rerank_links and all(score < 0.4 for score in combined_scores):
                 return 1
 
             for idx, doc in enumerate(reranked_documents):
